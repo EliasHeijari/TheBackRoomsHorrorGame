b94447ef8183a3c3df396055d0b1adb865202453
Elias Heijari <69460338+elkkuzq@users.noreply.github.com>
2023-11-28 16:21:37 +0200

    Slides Timelines

--- Assets/Scripts/SlideInteractable.cs
+++ Assets/Scripts/SlideInteractable.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
 public class SlideInteractable : MonoBehaviour, IInteractable
 {
-    [SerializeField] private string slide;
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] private int sceneBuildIndex = 2;
     public void Interact(Transform interactorTransform)
     {
+        playerController.enabled = false;
+        GetComponent<PlayableDirector>().Play();
+        GetComponent<PlayableDirector>().played += SlideInteractable_played;
+    }
 
+    private void SlideInteractable_played(PlayableDirector obj)
+    {
+        SceneManager.LoadScene(sceneBuildIndex);
     }
 
     public string GetInteractText()
