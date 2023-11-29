ad0d5cac342eb1cb1d29ccf69b065a964694748f
Elias Heijari <69460338+elkkuzq@users.noreply.github.com>
2023-11-29 15:55:04 +0200

    lights to yellow and post processing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -88,7 +88,7 @@ public class Player : MonoBehaviour
 
     IEnumerator LoadEndScene()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1.5f);
         SceneManager.LoadSceneAsync(EndSceneBuildIndex);
     }
 
